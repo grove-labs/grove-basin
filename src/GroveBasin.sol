cb2a4b193bc9638cbdbec56a9af00568dfba7d76
David Lee <david@grove.finance>
2026-03-13 08:52:56 -0400

    Initiate and complete redeem

--- src/GroveBasin.sol
+++ src/GroveBasin.sol
@@ -114,10 +114,11 @@ contract GroveBasin is IGroveBasin, AccessControl {
 
         _grantRole(OWNER_ROLE,              owner_);
         _grantRole(LIQUIDITY_PROVIDER_ROLE, msg.sender);
-        _setRoleAdmin(MANAGER_ROLE, MANAGER_ADMIN_ROLE);
+
+        _setRoleAdmin(MANAGER_ROLE,            MANAGER_ADMIN_ROLE);
         _setRoleAdmin(LIQUIDITY_PROVIDER_ROLE, MANAGER_ADMIN_ROLE);
-        _setRoleAdmin(REDEEMER_CONTRACT_ROLE, MANAGER_ADMIN_ROLE);
-        _setRoleAdmin(REDEEMER_ROLE,          MANAGER_ADMIN_ROLE);
+        _setRoleAdmin(REDEEMER_CONTRACT_ROLE,  MANAGER_ADMIN_ROLE);
+        _setRoleAdmin(REDEEMER_ROLE,           MANAGER_ADMIN_ROLE);
 
         // Necessary to ensure rounding works as expected
         require(_swapTokenPrecision  <= 1e18,      "GroveBasin/swapToken-precision-too-high");
